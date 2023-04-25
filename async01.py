b901400de658b9d34d769371064d7ff1950f2ad2
jtsizemore <jtsizemore@outlook.com>
2023-04-25 21:03:27 +0000

    dabeazley timestamp - 00:33:00 - basic threaded producer/consumer

--- async01.py
+++ async01.py
@@ -1,59 +1,34 @@
 #!/usr/bin/python3
 """
-dabeazley python: build your own async - timestamp 00:26:15
-the time.sleep() calls are blocking and prevent anything else from running
-how to address this blocking issue ?
-add a method to class Scheduler that schedules work to be run in the future.
+dabeazley python: build your own async - timestamp 00:33:00
+start producer/consumer
 """
+
+
 import time
-from collections import deque
+import queue
+import threading
 
 
-class Scheduler:
-    def __init__(self):
-        self.work_queue = deque()
-        self.future_work_queue = []
-
-    def work_queue_add(self, work_function):
-        self.work_queue.append(work_function)
+def producer(queue, count):
+    for n in range(1, (count + 1)):
+        print('Producing: ', n)
+        q.put(n)
+        time.sleep(1)
     
-    def future_work_queue_add(self, hold_delay, work_function):
-        target_run_time = time.time() + hold_delay
-        self.future_work_queue.append((target_run_time, work_function))
-        self.future_work_queue.sort()
-
-    def run(self):
-        while self.work_queue or self.future_work_queue:
-            if not self.work_queue:
-                target_run_time, work_function = self.future_work_queue.pop(0)
-                hold_wait = target_run_time - time.time()
-                if hold_wait > 0:
-                    time.sleep(hold_wait)
-                self.work_queue.append(work_function)
-            while self.work_queue:
-                work_function = self.work_queue.popleft()
-                work_function()
+    print('Producing --> Done')
+    q.put(None)
 
 
-work_scheduler = Scheduler()
+def consumer(q):
+    while True:
+        item = q.get()
+        if item is None:
+            break
+        print('Consuming: ', item)
+    print('Consumer --> Done')
 
 
-def countdown(n):
-    if n > 0:
-        print('Count Down: ', n)
-        work_scheduler.future_work_queue_add(4, lambda: countdown(n-1))
-
-
-def countup(stop_count):
-    def _internal_control_func(x):
-        if x < stop_count:
-            print('Count Up: ', x)
-            work_scheduler.future_work_queue_add(
-                1, lambda: _internal_control_func(x+1)
-                )
-    _internal_control_func(0)
-
-
-work_scheduler.work_queue_add(lambda: countdown(5))
-work_scheduler.work_queue_add(lambda: countup(20))
-work_scheduler.run()
+q = queue.Queue()
+threading.Thread(target=producer, args=(q, 10)).start()
+threading.Thread(target=consumer, args=(q,)).start()
